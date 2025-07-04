ee181cd1de3d5e89f58cfbf415d631d5a2fc6733
enzodmrib <enzorib2@gmail.com>
2025-07-04 17:02:19 -0300

    ref: adds redis as database

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"log/slog"
 	"net/http"
-	"rocketseat/api"
 	"time"
+
+	"rocketseat/internal/api"
+	"rocketseat/internal/store"
+
+	"github.com/redis/go-redis/v9"
 )
 
 func main() {
@@ -16,8 +20,14 @@ func main() {
 }
 
 func run() error {
-	db := make(map[string]string)
-	handler := api.NewHandler(db)
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	store := store.NewStore(rdb)
+	handler := api.NewHandler(store)
 
 	s := http.Server{
 		ReadTimeout:  time.Second * 10,
